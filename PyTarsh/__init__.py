497cea90a283b6a8f19b55e13685f432173a8bec
OmarMohsen010 <omar.dimohamed88@gmail.com>
2025-12-03 20:37:34 +0200

    showiet cleaning lel init file

--- PyTarsh/__init__.py
+++ PyTarsh/__init__.py
@@ -5,23 +5,34 @@ Neural Network Library called PyTarsh aw PyZart
 from .layers import Layer, Dense
 from .activations import ReLU, Sigmoid, Tanh, Softmax
 from .losses import mse_loss, mse_gradient, MSELoss
-from .optimizer import SGD, SGDMomentum
-from .network import Sequential
+from .optimizers import SGD, SGDMomentum
+from .network import Sequential #lesa bit3ml#
 
 __version__ = "1.0.0"
 __author__ = "Omar Mohsen,Youssf Mostafa,Ahmed Abdallah"
 
 __all__ = [
+    # Layers
     'Layer',
     'Dense',
+    
+    # Activations
     'ReLU',
     'Sigmoid',
     'Tanh',
     'Softmax',
+    'Linear',
+    'get_activation',
+    
+    # Losses
     'mse_loss',
     'mse_gradient',
     'MSELoss',
+    
+    # Optimizers
     'SGD',
     'SGDMomentum',
+    
+    # Models
     'Sequential'
 ]
